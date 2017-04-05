0f0bd6dd23295af635df27c3425123ba7dd8fe14
Adrian Melo <amelo2@fvi.edu>
2017-04-05 17:00:18 -0400

    finish

--- js/Enviroment.js
+++ js/Enviroment.js
@@ -10,12 +10,12 @@ this.bgImg = document.getElementById('bg');
 this.bgImage = document.getElementById('world');
 }
 Enviroment.prototype.update = function() {
-  this.bgPos -= this.bgSpeed;
+  // this.bgPos -= this.bgSpeed;
   if(this.bgPos < -this.bgWidth)
   this.bgPos = 0;
 };
 Enviroment.prototype.render = function(){
   for(let i = 0; i <= this.c.width/this.bgWidth+1; i++)
   this.ctx.drawImage(this.bgImg, this.bgPos+i*this.bgWidth, 0);
-  this.ctx.drawImage(this.bgImage, this.bgWidth/2, this.bgheight/2);
+  this.ctx.drawImage(this.bgImage, this.bgWidth/2, this.bgheight/4);
 };
--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 window.onload= function(){
   const c = document.getElementById('canvas');
   c.width = window.innerWidth;
-  c.height = 509;
-var context = "";
+  c.height = window.innerHeight;
+  var context = "";
   var data = [];
 
+
   const ctx = c.getContext('2d');
-const enviroment = new Enviroment(c, ctx);
+  const enviroment = new Enviroment(c, ctx);
 
    $.ajax({
         url: "http://api.open-notify.org/astros.json",
@@ -18,14 +19,17 @@ const enviroment = new Enviroment(c, ctx);
 
     });
 
-gameloop();
 
-
-
+    gameloop();
+    var r = 0;
    function gameloop(){
      ctx.fillRect(0,0,c.width, c.height);
      enviroment.update();
      enviroment.render();
+     ctx.save();
+     ctx.translate(window.innerWidth/2,window.innerHeight/2);
+     ctx.rotate(r / 800);
+     ctx.translate(-290, 0);
      ctx.font= "14px Georgia";
      ctx.fillStyle = "white";
      for(var i = 0; i < data.length; i++){
@@ -33,6 +37,11 @@ gameloop();
           ctx.fillText(context, enviroment.bgWidth/2,100 + 20 * i);
         }
 
+        ctx.restore();
+
+        r++;
+
      window.requestAnimationFrame(gameloop);
    }
+   window.requestAnimationFrame(gameloop);
 }
